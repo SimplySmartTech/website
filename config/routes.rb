e65a139794f1574cdb97e7b15b4c8e0e3a966083
Shailesh Patil <shailesh@joshsoftware.com>
2016-02-09 15:03:11 +0530

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'solutions/:for'  => 'home#solutions', as: :solution
+  get 'smart_city'      => 'home#smart_city'
+  get 'management_team' => 'home#management_team'
+  get 'investors'       => 'home#investors'
+  get 'contact_us'      => 'home#contact_us'
+
+  get 'case_studies(/:company)'    => 'home#case_studies', as: :case_studies
+  root "home#index"
+end
