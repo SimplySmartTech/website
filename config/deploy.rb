76ba11050ad5fad2c2f48018a29b50974e14c0de
Shailesh Patil <shailesh@joshsoftware.com>
2016-03-02 15:08:26 +0530

    symlink creds

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :user, 'ubuntu'
 
 # Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
 # They will be linked in the 'deploy:link_shared_paths' step.
-set :shared_paths, ['config/database.yml', 'log', 'tmp']
+set :shared_paths, ['config/database.yml', 'log', 'tmp', 'config/credentials.yml']
 
 # Optional settings:
 #   set :user, 'foobar'    # Username in the server to SSH to.
@@ -56,6 +56,9 @@ task :setup => :environment do
 
   queue! %[touch "#{deploy_to}/#{shared_path}/config/database.yml"]
   queue  %[echo "-----> Be sure to edit '#{deploy_to}/#{shared_path}/config/database.yml'."]
+
+  queue! %[touch "#{deploy_to}/#{shared_path}/config/credentials.yml"]
+  queue  %[echo "-----> Be sure to edit '#{deploy_to}/#{shared_path}/config/credentials.yml'."]
 end
 
 desc "Deploys the current version to the server."
