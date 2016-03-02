b904494319e803eb0e8909021fc91ba7cd81e9f3
Shailesh Patil <shailesh@joshsoftware.com>
2016-03-02 15:05:56 +0530

    changed emails

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -3,7 +3,7 @@ class Notifier < ApplicationMailer
   def enquiry(params)
     @params = params
     mail(
-      to: "shailesh@joshsoftware.com",#["gautam@simplysmart.tech", "sethu@simplysmart.tech", "avadhoot@simplysmart.tech"] 
+      to: ["gautam@simplysmart.tech", "sethu@simplysmart.tech", "avadhoot@simplysmart.tech"] 
       subject: "New Enquiry: #{params[:subject]}"
     )
   end
